9fc7f03ee6a820b9b246361d7af0af38d1770e49
Bhavnayadavv <bhavnayadav68752@gmail.com>
2023-10-14 21:47:23 +0530

    first commit

---
+++ src/components/MiddleContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MiddleContent.css'
+
+const MiddleContent = () => {
+  return (
+    <div className="content">
+      <h1 className='dynamic-font'>The Biggest Indian hits. Ready to Watch here from  ₹149.</h1>
+   <p>Join today. Cancel anytime.</p>
+   <p>Ready to Watch? Enter your email to create or restart your membership.</p>
+    </div>
+  )
+}
+
+export default MiddleContent
